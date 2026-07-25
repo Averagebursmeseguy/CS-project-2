4f9cd0158a4bfc3d47814a3d86702806d67c0e40
Averagebursmeseguy <115561450+Averagebursmeseguy@users.noreply.github.com>
2026-07-25 10:29:12 +0800

    Made the database creation script

--- dbman.py
+++ dbman.py
@@ -1,4 +1,29 @@
 import sqlite3
+import os
+
 con = sqlite3.connect("app.db")
+con.execute("PRAGMA foreign_keys = ON;")
 
 cursor = con.cursor()
+
+cursor.execute("""CREATE TABLE users(
+user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+ name TEXT NOT NULL,
+ email TEXT NOT NULL UNIQUE
+);""")
+
+cursor.execute("""CREATE TABLE habits(
+habit_id INTEGER PRIMARY KEY AUTOINCREMENT,
+set_by_id INTEGER NOT NULL,
+title TEXT NOT NULL,
+timespan TEXT NOT NULL,
+FOREIGN KEY (set_by_id) REFERENCES users(user_id) ON DELETE CASCADE
+)""")
+
+cursor.execute("""CREATE TABLE goals(
+goal_id INTEGER PRIMARY KEY AUTOINCREMENT,
+set_by_id INTEGER NOT NULL,
+title TEXT NOT NULL,
+state TEXT NOT NULL,
+FOREIGN KEY (set_by_id) REFERENCES users(user_id) ON DELETE CASCADE
+);""")
